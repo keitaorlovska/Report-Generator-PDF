7ab46f6da9270dce4a4d3fadb46e92da4c3d3e01
keitaorlovska <keitaorlovska@gmail.com>
2026-02-25 18:05:30 +0100

    fix next config

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Newer Next.js
-  serverExternalPackages: ["pdfkit"],
-
-  // Older Next.js (still used in some versions)
   experimental: {
     serverComponentsExternalPackages: ["pdfkit"],
   },
